3210aab82f5caed20adccd640ede1ad5109a6b4f
saksham8871 <148368704+saksham8871@users.noreply.github.com>
2024-09-12 17:59:06 +0000

    json talk

---
+++ basic/objects.js
@@ -0,0 +1,9 @@
+const course = {
+    name: "js in hindi",
+    Courseinstructor: "saksham",
+    price: "999"
+}
+
+//course.Courseinstructor
+const{Courseinstructor: instructor} = course;
+console.log(instructor);
