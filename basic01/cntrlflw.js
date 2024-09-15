152b0be17c917fd4425be769d05f1c4caa061c3c
saksham8871 <148368704+saksham8871@users.noreply.github.com>
2024-09-15 15:08:38 +0000

    jason talk

---
+++ basic01/cntrlflw.js
@@ -0,0 +1,48 @@
+// In == it not check data type;
+
+// console.log(2 == "2"); // return true
+
+// // In === it check the data type strictly
+
+// console.log(2 === "2");  // It return false
+
+// // In != it not check data type
+
+// console.log(2 != "2");  // return true
+
+// console.log(2 !== "2");
+
+
+
+
+
+
+
+
+// FALSY Value
+
+   // false , 0 , -0 ,  BigInt 0n , "" , null , undefined, Nan;
+
+
+//    const emptyobj = {};
+
+//    if(Object.keys(emptyobj).length === 0) {
+//     console.log("Ha Khali Hai");
+    
+//    }
+
+
+// Nullish coalesing operator (??) : null undefined
+
+let val1;
+val1 = 5 ?? 10;
+
+
+console.log((val1));
+
+
+
+
+
+
+
